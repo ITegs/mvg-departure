847f2fce3bbd4e541989f42eedc95b4fac5c9c59
Johannes Pahle <joe@jo-dev.de>
2023-03-03 11:56:22 +0100

    🚑 minor hotfixes

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ root.render(
   <React.StrictMode>
     {window.innerWidth > 1400 ? (
       <div className="qr-code">
-        <img src="qr.png" />
+        <img src="qr.png" alt="qrcode" />
         <h2>Diese App ist optimiert für mobile Geräte!</h2>
         <p>
           Scan den Qr-Code mit deinem Smartphone um MVG-departure nutzen zu
