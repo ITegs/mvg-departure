6d6e04033c4702821948b1bca5f7c0ca29cdc1e7
Johannes Pahle <joe@jo-dev.de>
2023-03-03 11:49:12 +0100

    ✨ show qr code on nonmobile

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,17 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {window.innerWidth > 1400 ? (
+      <div className="qr-code">
+        <img src="qr.png" />
+        <h2>Diese App ist optimiert für mobile Geräte!</h2>
+        <p>
+          Scan den Qr-Code mit deinem Smartphone um MVG-departure nutzen zu
+          können.
+        </p>
+      </div>
+    ) : (
+      <App />
+    )}
   </React.StrictMode>
 );
