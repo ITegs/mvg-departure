f1dc6600961febc6a47dbf1a637518dd57010511
Johannes Pahle <joe@jo-dev.de>
2023-06-13 17:43:51 +0200

    🐛 parse API correctly

--- src/components/DepartCard.tsx
+++ src/components/DepartCard.tsx
@@ -5,16 +5,13 @@ import { Departure } from "../types";
 export default function DepartCard(dep: Departure) {
   return (
     <div className="DCWrapper">
-      <div
-        className="DCLine"
-        style={{ backgroundColor: dep.lineBackgroundColor }}
-      >
-        {dep.label}
+      <div className="DCLine" style={{ backgroundColor: "black" }}>
+        {dep.line.number}
         {" - "}
-        {dep.destination}
+        {dep.line.direction}
       </div>
       <div className="DCTime">
-        in <b>{dep.departureTime + dep.delay}</b> min
+        in <b>{dep.remaining}</b> min
       </div>
     </div>
   );
