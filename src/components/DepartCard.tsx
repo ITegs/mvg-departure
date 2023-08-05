12b1d1b3ea778dd6e492ffcbee2926630d5d16f4
Johannes Pahle <joe@jo-dev.de>
2023-08-05 21:06:15 +0200

    🔨 show currently departing trains as `now`

--- src/components/DepartCard.tsx
+++ src/components/DepartCard.tsx
@@ -5,7 +5,9 @@ import { Departure } from "../types";
 export default function DepartCard(props: { dep: Departure; i: number }) {
   return (
     <div
-      className="DCWrapper"
+      className={
+        "DCWrapper" + (props.dep.remaining > 0 ? "" : " DCNowOutlined")
+      }
       style={{ animationDelay: props.i * 0.15 + 0.8 + "s" }}
     >
       <div className="DCLine" style={{ backgroundColor: props.dep.color }}>
@@ -13,9 +15,16 @@ export default function DepartCard(props: { dep: Departure; i: number }) {
         {" - "}
         {props.dep.line.direction}
       </div>
-      <div className="DCTime">
-        in <b>{props.dep.remaining}</b> min
-      </div>
+      {props.dep.remaining > 0 ? (
+        <div className="DCTime">
+          in <b>{props.dep.remaining}</b> min
+        </div>
+      ) : (
+        <div className="DCNow">
+          <div className="DCDot"></div>
+          Jetzt
+        </div>
+      )}
     </div>
   );
 }
